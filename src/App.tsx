f79a0e0c967c14728271a81e26864551636ade41
Roman Lysunets <rlysunets@gmail.com>
2026-05-27 15:57:53 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Route, Routes, Navigate, NavLink } from 'react-router-dom';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
+import { HomePage } from './pages/HomePage';
+import { TabsPage } from './pages/TabsPage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import classNames from 'classnames';
 
 export const App = () => (
   <>
@@ -17,39 +17,40 @@ export const App = () => (
     >
       <div className="container">
         <div className="navbar-brand">
-          <a href="/" className="navbar-item is-active">
+          <NavLink
+            to="/"
+            className={({ isActive }) => {
+              return classNames('navbar-item', { 'is-active': isActive });
+            }}
+          >
             Home
-          </a>
-          <a href="/tabs" className="navbar-item">
+          </NavLink>
+
+          <NavLink
+            to="/tabs"
+            className={({ isActive }) => {
+              return classNames('navbar-item', { 'is-active': isActive });
+            }}
+          >
             Tabs
-          </a>
+          </NavLink>
         </div>
       </div>
     </nav>
 
     <div className="section">
       <div className="container">
-        <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
 
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
+          <Route path="tabs" element={<TabsPage />}>
+            <Route path=":tabId" element={<TabsPage />} />
+          </Route>
 
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div>
+          <Route path="home" element={<Navigate to="/" replace />} />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </div>
     </div>
   </>
